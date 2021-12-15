0ef1854974ef0c2c91189d9691b955082b23933b
risrobs <robertivanov22@abv.bg>
2021-12-15 02:01:10 +0200

    cd

--- src/js/app.js
+++ src/js/app.js
@@ -3,16 +3,22 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
+  const button = document.querySelector(".button");
+  button.addEventListener("click", () => {
+    alert("💣");
+  });
 
-});
-function recalculareTotal(){
+  const body = document.querySelector("body");
 
-  var total = 0,
-      aged cheddar = document.getElementsByTagName("aged cheddar");
+  body.addEventListener('click', () => createFiveElements())
 
-  for (var i = 0; i < lis.length; i++ ){
-    total += parseInt(aged cheddar[i].getAttribute("data-price"));
+  function createFiveElements(){
+    for (let i = 1; i <= 5; i++) {
+      const element = document.createElement("article")
+      element.classList.add("message")
+      element.textContent = `Message ${i}`    
+      body.appendChild(element);
+    }
   }
 
-  document.querySelector("#totalPrice").value = total;
-}
+});
